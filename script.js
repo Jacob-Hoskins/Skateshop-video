0df25cec9160180863a0a384c89c82a3649d30c4
Jacob Hoskins <45977627+Jacob-Hoskins@users.noreply.github.com>
2023-02-17 14:39:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const clothesCarouselElement = document.querySelector("#clothesCarousel");
+
+const carousel = new bootstrap.Carousel(clothesCarouselElement, {
+  interval: 2500,
+  touch: false,
+});
