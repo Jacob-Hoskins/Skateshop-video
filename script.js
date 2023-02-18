6bffdc87dcbaee08c535a37c8ffe76a042dc4d0c
Jacob Hoskins <45977627+Jacob-Hoskins@users.noreply.github.com>
2023-02-18 14:09:23 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const clothesCarouselElement = document.querySelector("#clothesCarousel");
+const gearCarouselElement = document.querySelector("#gearCarousel");
+const waxCarouselElement = document.querySelector("#waxCarousel");
 
-const carousel = new bootstrap.Carousel(clothesCarouselElement, {
+const clothes_carousel = new bootstrap.Carousel(clothesCarouselElement, {
+  interval: 2500,
+  touch: false,
+});
+
+const gear_carousel = new bootstrap.Carousel(gearCarouselElement, {
+  interval: 2500,
+  touch: false,
+});
+
+const wax_carousel = new bootstrap.Carousel(waxCarouselElement, {
   interval: 2500,
   touch: false,
 });
